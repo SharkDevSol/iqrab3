ddf3d5d5e71fdf9066c412c71c4af5929b893ad7
kamilibrahim877-glitch <kamilibrahim877@example.com>
2026-04-02 02:06:22 -0700

    fix: student list buttons inline with name on mobile

--- APP/src/PAGE/Schedule/TestSchedule.jsx
+++
@@ -1,8 +0,0 @@
-// src/PAGE/Schedule/TestSchedule.jsx
-import React from 'react';
-
-const TestSchedule = () => {
-  return <div>Test Schedule - Working!</div>;
-};
-
-export default TestSchedule;
--- fix_marklist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/www/bilal-school/backend/routes
-cp markListRoutes.js markListRoutes.js.backup
-
-# Fix the regex to include period
-sed -i "s/replace(\\/\[\\\\s\\\\-\]+\\/g, '_')/replace(\\/[\\\\s\\\\-\\\\.]+\\/g, '_')/g" markListRoutes.js
-
-echo "Fixed regex pattern"
